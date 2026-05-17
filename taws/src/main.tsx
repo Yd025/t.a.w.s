9004a44d7b53a7fa5f27b132db63ff01132f4e28
Oscar Shijie Song <oscarshijiesong@gmail.com>
2026-05-16 19:10:08 -0700

    added amplify.yml

--- taws/src/main.tsx
+++ taws/src/main.tsx
@@ -1,5 +1,9 @@
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import './styles/index.css'
+
+Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')!).render(<App />)
